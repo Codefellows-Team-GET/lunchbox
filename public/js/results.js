eeb365a09d46e8bcdca95bed4382401f21f6e925
seasouthern <matthew.james.johnson@hotmail.com>
2020-01-30 10:55:31 -0800

    fixed results table header, working on sort

--- public/js/results.js
+++ public/js/results.js
@@ -1,5 +1,12 @@
 'use strict';
 
-
 console.log('JS linked');
 
+$(".sort").click(function(){
+  console.log('something was clicked', $(this).text())
+  $.ajax({
+    type: "POST",
+    url: "/sort",
+    data: $(this).text(),
+    dataType: "JSON"
+});
